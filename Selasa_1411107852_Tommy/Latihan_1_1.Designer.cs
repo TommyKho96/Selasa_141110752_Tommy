b447802b60d3f5dde0066798b854eab26857652a
TommyKho96 <tommy.y1231@gmail.com>
2016-11-01 06:00:43 +0700

    first commit

---
+++ Selasa_1411107852_Tommy/Latihan_1_1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Selasa_1411107852_Tommy
+{
+    partial class Latihan_1_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.TextBox();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.label1 = new System.Windows.Forms.TextBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(266, 246);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.vScrollBar2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.vScrollBar1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(258, 220);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Information";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(258, 220);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Help";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(205, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            this.label2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.LargeChange = 1;
+            this.vScrollBar2.Location = new System.Drawing.Point(205, 40);
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(17, 155);
+            this.vScrollBar2.TabIndex = 6;
+            this.vScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(36, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.LargeChange = 1;
+            this.vScrollBar1.Location = new System.Drawing.Point(36, 40);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 155);
+            this.vScrollBar1.TabIndex = 4;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // Latihan_1_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 266);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Latihan_1_1";
+            this.Text = "Latihan_1_1";
+            this.Load += new System.EventHandler(this.Latihan_1_1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox label2;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.TextBox label1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ Selasa_1411107852_Tommy/Latihan_1_1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Selasa_1411107852_Tommy
+{
+    public partial class Latihan_1_1 : Form
+    {
+        public Latihan_1_1()
+        {
+            InitializeComponent();
+            vScrollBar1.Minimum = 0;
+            vScrollBar1.Maximum = 20;
+            vScrollBar2.Minimum = 0;
+            vScrollBar2.Maximum = 20;
+
+            vScrollBar1.Value = 0;
+            vScrollBar2.Value = 20;
+
+        }
+
+        private void Latihan_1_1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "" + vScrollBar1.Value.ToString();
+            label2.Text = "" + vScrollBar2.Value.ToString();
+
+            ToolTip tooltip = new ToolTip();
+            tooltip.AutoPopDelay = 3000;
+            tooltip.InitialDelay = 500;
+            tooltip.ReshowDelay = 100;
+
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            int tempbertambah;
+            tempbertambah = vScrollBar1.Value - 1;
+            int temp;
+            temp = vScrollBar1.Value;
+            int tempberkurang;
+            tempberkurang = vScrollBar1.Value + 1;
+            label1.Text = "" + vScrollBar1.Value.ToString();
+
+            if (vScrollBar1.Value > vScrollBar1.Minimum)
+            {
+                if (vScrollBar2.Value > 0)
+                {
+                    vScrollBar2.Value -= 1;
+                    label2.Text = "" + vScrollBar2.Value.ToString();
+                }
+                else
+                {
+                    vScrollBar2.Value += 0;
+                    label2.Text = "" + vScrollBar2.Value.ToString();
+                }
+
+            }
+
+
+            else if (vScrollBar1.Value == vScrollBar1.Maximum)
+            {
+                vScrollBar2.Value += 1;
+                label2.Text = "" + vScrollBar2.Value.ToString();
+            }
+
+        }
+
+        private void vScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            label2.Text = "" + vScrollBar2.Value.ToString();
+        }
+    }
+}
